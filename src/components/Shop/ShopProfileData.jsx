e02aae4d43aec1614015d93b40df640e4193e4ab
nrasel <nrasel452@gmail.com>
2023-06-22 01:22:03 +0600

    seller info and dashboard

---
+++ src/components/Shop/ShopProfileData.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { productData } from "../../static/data";
+import styles from "../../styles/styles";
+import ProductCard from "../Route/ProductCard/ProductCard";
+
+const ShopProfileData = ({ isOwner }) => {
+  const [active, setActive] = useState(1);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <div className="w-full flex">
+          <div onClick={() => setActive(1)} className="flex items-center">
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 1 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Shop Products
+            </h5>
+          </div>
+          <div onClick={() => setActive(2)} className="flex items-center">
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 2 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Running Events
+            </h5>
+          </div>
+          <div onClick={() => setActive(3)} className="flex items-center">
+            <h5
+              className={`font-[600] text-[20px] ${
+                active === 3 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
+            >
+              Shop Reviews
+            </h5>
+          </div>
+        </div>
+        <div>
+          {isOwner && (
+            <div>
+              <Link to="/dashboard">
+                <div className={`${styles.button} !rounded-[4px] h-[42px]`}>
+                  <span className="text-[#fff]">Go Dashboard</span>
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+      <br />
+      <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+        {productData &&
+          productData.map((i, index) => (
+            <ProductCard data={i} key={index} isShop={true} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShopProfileData;
--- src/routes/SellerProtectedRoute.js
+++ src/routes/SellerProtectedRoute.js
@@ -1,11 +1,14 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import Loader from "../components/Layout/Loader";
 
 const SellerProtectedRoute = ({ children }) => {
   const { isSellerAuthenticated, isLoading } = useSelector(
     (state) => state.seller
   );
-  if (isLoading === false) {
+  if (isLoading === true) {
+    return <Loader />;
+  } else {
     if (!isSellerAuthenticated) {
       return <Navigate to={`/shop-login`} replace />;
     }
