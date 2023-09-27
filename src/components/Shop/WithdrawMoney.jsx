b9981efa00c2976066d8c34b33db25fd50797d8f
nrasel <nrasel452@gmail.com>
2023-09-28 01:17:35 +0600

    withdraw request

--- src/components/Shop/WithdrawMoney.jsx
+++ src/components/Shop/WithdrawMoney.jsx
@@ -1,7 +1,10 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import { getAllOrdersOfShop } from "../../redux/actions/order";
+import { server } from "../../server";
 import styles from "../../styles/styles";
 
 const WithdrawMoney = () => {
@@ -11,6 +14,14 @@ const WithdrawMoney = () => {
   const [open, setOpen] = useState(false);
   const [deliveredOrder, setDeliveredOrder] = useState(null);
   const [paymentMethod, setPaymentMethod] = useState(false);
+  const [bankInfo, setBankInfo] = useState({
+    bankName: "",
+    bankCountry: "",
+    bankSwiftCode: null,
+    bankAccountNumber: null,
+    bankHolderName: "",
+    bankAddress: "",
+  });
 
   useEffect(() => {
     dispatch(getAllOrdersOfShop(seller?._id));
@@ -24,6 +35,30 @@ const WithdrawMoney = () => {
     deliveredOrder.reduce((acc, item) => acc + item.totalPrice, 0);
   const serviceCharge = totalEarningWithoutTax * 0.1;
   const availableBalance = (totalEarningWithoutTax - serviceCharge).toFixed(2);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const withdrawMethod = {
+      bankName: bankInfo.bankName,
+      bankCountry: bankInfo.bankCountry,
+      bankSwiftCode: bankInfo.bankSwiftCode,
+      bankAccountNumber: bankInfo.bankAccountNumber,
+      bankHolderName: bankInfo.bankHolderName,
+      bankAddress: bankInfo.bankAddress,
+    };
+    await axios
+      .put(
+        `${server}/shop/update-payment-method`,
+        {
+          withdrawMethod,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success("withdraw method added successfully!");
+      });
+  };
 
   return (
     <div className="w-full h-[90vh] p-8">
@@ -40,7 +75,11 @@ const WithdrawMoney = () => {
       </div>
       {open && (
         <div className="w-full fixed top-0 left-0 flex  items-center justify-center bg-[#0000004e] h-screen z-[9999]">
-          <div className="w-[95%] 800px:w-[50%] bg-white shadow rounded min-h-[40vh] p-3">
+          <div
+            className={`w-[95%] 800px:w-[50%] bg-white shadow rounded  min-h-[40vh] p-3 ${
+              paymentMethod ? "h-[60vh] overflow-y-scroll" : "h-[unset]"
+            }`}
+          >
             <div className="w-full flex justify-end">
               <RxCross1
                 size={25}
@@ -49,8 +88,139 @@ const WithdrawMoney = () => {
               />
             </div>
             {paymentMethod ? (
-              <div className="text-[22px] font-Poppins text-center font-[600]">
-                Add new Payment Method
+              <div>
+                <h3 className="text-[22px] font-Poppins text-center font-[600]">
+                  Add new Payment Method:
+                </h3>
+                <form onSubmit={handleSubmit} action="">
+                  <div>
+                    <label htmlFor="">
+                      Bank Name <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name=""
+                      id=""
+                      placeholder="Enter your bank name!"
+                      className={`${styles.input} mt-2`}
+                      required
+                      value={bankInfo.bankName}
+                      onChange={(e) =>
+                        setBankInfo({ ...bankInfo, bankName: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="pt-2">
+                    <label htmlFor="">
+                      Bank Country
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name=""
+                      id=""
+                      placeholder="Enter your bank country!"
+                      className={`${styles.input} mt-2`}
+                      required
+                      value={bankInfo.bankCountry}
+                      onChange={(e) =>
+                        setBankInfo({
+                          ...bankInfo,
+                          bankCountry: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+
+                  <div className="pt-2">
+                    <label htmlFor="">
+                      Bank Swift Code
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name=""
+                      id=""
+                      placeholder="Enter your bank swift code!"
+                      className={`${styles.input} mt-2`}
+                      required
+                      value={bankInfo.bankSwiftCode}
+                      onChange={(e) =>
+                        setBankInfo({
+                          ...bankInfo,
+                          bankSwiftCode: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className="pt-2">
+                    <label htmlFor="">
+                      Bank Account Number
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name=""
+                      id=""
+                      placeholder="Enter your bank account number!"
+                      className={`${styles.input} mt-2`}
+                      required
+                      value={bankInfo.bankAccountNumber}
+                      onChange={(e) =>
+                        setBankInfo({
+                          ...bankInfo,
+                          bankAccountNumber: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+
+                  <div className="pt-2">
+                    <label htmlFor="">
+                      Bank Holder Name
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name=""
+                      id=""
+                      placeholder="Enter your bank holder name!"
+                      className={`${styles.input} mt-2`}
+                      required
+                      value={bankInfo.bankHolderName}
+                      onChange={(e) =>
+                        setBankInfo({
+                          ...bankInfo,
+                          bankHolderName: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className="pt-2">
+                    <label htmlFor="">
+                      Bank Address
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      name=""
+                      id=""
+                      placeholder="Enter your bank address!"
+                      className={`${styles.input} mt-2`}
+                      required
+                      value={bankInfo.bankAddress}
+                      onChange={(e) =>
+                        setBankInfo({
+                          ...bankInfo,
+                          bankAddress: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <button className={`${styles.button} mb-3 mt-6 text-white`}>
+                    Add
+                  </button>
+                </form>
               </div>
             ) : (
               <>
@@ -80,5 +250,7 @@ const WithdrawMoney = () => {
     </div>
   );
 };
+
+// start from 3:03 minute
 
 export default WithdrawMoney;
